c9e9d358e9422493cdb203716d3c1bfec3c11104
onichandame <zxinmyth@gmail.com>
2020-01-15 22:53:34 +0800

    staged

--- config/default.ts
+++ config/default.ts
@@ -1,37 +1,42 @@
-module.exports={
-  name:'TableJobs',
-  cols:{
-    submitted_at:{
-      type:'int',
-      notnull:true
-    },
-    timeout:{
-      type:'int',
-      notnull:true,
-      check:'timeout > 0'
-    },
-    project:{
-      type:'text',
-      notnull:true
-    },
-    engine:{
-      type:'text',
-      check:`engine LIKE 'xelatex' OR engine LIKE 'pdflatex'`
-    },
-    main:{
-      type:'text',
-      notnull:true
-    },
-    status:{
-      type:'int',// -1: failed; 0: job done; 1: calculating; 2: received
-      notnull:true,
-      check:'status < 3'
-    },
-    started_at:{
-      type:'int'
-    },
-    finished_at:{
-      type:'int'
+import * as path from 'path'
+
+export default {
+  root: path.dirname(__dirname),
+  database: {
+    cols: {
+      submitted_at: {
+        type:'int',
+        notnull:true
+      },
+      timeout: {
+        type:'int',
+        notnull:true,
+        check:'timeout > 0'
+      },
+      project: {
+        type:'text',
+        notnull:true
+      },
+      engine: {
+        type:'text',
+        check:`engine LIKE 'xelatex' OR engine LIKE 'pdflatex'`
+      },
+      main: {
+        type:'text',
+        notnull:true
+      },
+      status: {
+        type:'int',// -1: failed; 0: job done; 1: calculating; 2: received
+        notnull:true,
+        check:'status < 3'
+      },
+      started_at: {
+        type:'int'
+      },
+      finished_at: {
+        type:'int'
+      }
     }
-  }
+  },
+  name: 'TableJobs',
 }
